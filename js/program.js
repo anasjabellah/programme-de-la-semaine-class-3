5713fbb5fd259c9ed89bb107679f210cca7e7679
anas jabellah <jablcodiing@gmail.com>
2022-01-05 11:42:40 +0100

    edit page home

---
+++ js/program.js
@@ -0,0 +1,104 @@
+var days = 
+[
+    {
+        "name": "Monday",
+        "tasks": 
+        [
+            {
+                "start_time"    : "08:30",
+                "end_time"      : "08:45",
+                "name"          : "Stand Up Meeting",
+            }
+        ]
+    },
+    {
+        "name": "Tuesday",
+        "tasks": 
+        [
+            {
+                "start_time"    : "13:30",
+                "end_time"      : "14:45",
+                "name"          : "Stand Up Meeting",
+            },
+            {
+                "start_time"    : "14:45",
+                "end_time"      : "15:00",
+                "name"          : "Veille Tech",
+            },
+            {
+                "start_time"    : "09:00",
+                "end_time"      : "10:30",
+                "name"          : "Algorhythm",
+            },
+            {
+                "start_time"    : "10:30",
+                "end_time"      : "10:45",
+                "name"          : "Coffee Break",
+            },
+            {
+                "start_time"    : "10:45",
+                "end_time"      : "12:00",
+                "name"          : "Brief & Documentation",
+            },
+            {
+                "start_time"    : "12:00",
+                "end_time"      : "13:30",
+                "name"          : "Lunch Time",
+            }
+
+
+
+
+
+
+        ]
+    },
+    {
+        "name": "Wednesday",
+        "tasks": 
+        [
+            {
+                "start_time"    : "08:30",
+                "end_time"      : "08:45",
+                "name"          : "Stand Up Meeting",
+            },
+            {
+                "start_time"    : "08:45",
+                "end_time"      : "09:00",
+                "name"          : "Veille Tech",
+            },
+            {
+                "start_time"    : "09:00",
+                "end_time"      : "10:12",
+                "name"          : "Algorhythm",
+            },
+            {
+                "start_time"    : "10:12",
+                "end_time"      : "10:45",
+                "name"          : "Coffee Break",
+            },
+            {
+                "start_time"    : "10:45",
+                "end_time"      : "12:21",
+                "name"          : "Brief & Documentation",
+            },
+            {
+                "start_time"    : "12:21",
+                "end_time"      : "12:22",
+                "name"          : "Lunch Time",
+            }
+
+        ]
+    },
+    {
+        "name": "Thursday",
+        "tasks": 
+        [
+            {
+                "start_time"    : "08:30",
+                "end_time"      : "08:45",
+                "name"          : "Stand Up Meeting",
+            }
+        ]
+    }
+];
